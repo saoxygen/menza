b2904d541357d8022181f6eaa8d6c3f04d2e9ace
saoxygen <104069898+saoxygen@users.noreply.github.com>
2024-09-08 22:53:49 +0200

    Update services.js

--- services.js
+++ services.js
@@ -2,7 +2,7 @@ function serviceSwitch(id) {
   const divs = document.querySelectorAll("#full_service .service");
 
   divs.forEach((div, index) => {
-    console.log(div);
+    //console.log(div);
     if (!divs[index].classList.toString().includes("hideDivs")) {
       divs[index].classList.add("hideDivs");
     }
